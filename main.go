07ec5d36398d19e33c4ab176e63c8fca6c21aada
Totoketchup <anthony.damato@hotmail.fr>
2015-09-03 01:28:09 +0200

    Revert "Reduce drasticaly the length of request method"
    This reverts commit 7d11844a03d1a47142a256602172072dc574b988.

--- main.go
+++ main.go
@@ -20,7 +20,6 @@ import (
 
 	type ActionFunction func(string, map[string]interface{}) // func(entityID,input:'json')
 
-	type Knowledge map[string]interface {}
 
 	// INFO
 	
@@ -37,11 +36,9 @@ import (
 	var sysou bool
 
 
-
-func Request(requestType string, url string,JSONbody interface{}) []byte {
-	jsonStr, err := json.Marshal(JSONbody)
-
-	request, err := http.NewRequest(requestType, url, bytes.NewBuffer(jsonStr))
+func sendSuccess(requestID string, output string){
+	var jsonStr = []byte(output)
+	request, err := http.NewRequest("POST", httpURL + "/" + instanceID + "/actions/" + requestID + "/success", bytes.NewBuffer(jsonStr))
 	if err != nil {panic(err)}
 	request.Header.Set("content-type", "application/json; charset=utf-8");
 	request.Header.Set("accept", "");
@@ -51,37 +48,30 @@ func Request(requestType string, url string,JSONbody interface{}) []byte {
     resp, err := client.Do(request)
     if err != nil {panic(err)}
     defer resp.Body.Close()
-   	body, _ := ioutil.ReadAll(resp.Body)
     if(sysou){
+	    body, _ := ioutil.ReadAll(resp.Body)
+	    fmt.Println("Send success from action request Id :"+requestID)
 	    fmt.Println("response Status:", resp.Status)
 	    fmt.Println("response Headers:", resp.Header)
 	    fmt.Println("response Body:", string(body))
 	}
-	return body 
 }
 
-func sendSuccess(requestID string, output string){
-	Request("POST", httpURL + "/" + instanceID + "/actions/" + requestID + "/success", output)
-}
-
-func sendCancel(requestID string){
-	Request("POST", httpURL + "/" + instanceID + "/actions/" + requestID + "/cancelation", "")
-}
 
 
 func Light(requestID string, input map[string]interface{}) {
 	fmt.Println("Light On !! (start)")
-//	fmt.Println("My name is ", input["name"])
-//	fmt.Println("My height is ", input["height"])
+	fmt.Println("My name is ", input["name"])
+	fmt.Println("My height is ", input["height"])
 
-/*	var nice string
+	var nice string
 	if(input["nice"].(bool)){
 		nice = "YES"
 	} else {
 		nice ="NO"
 	}
 
-	fmt.Println("Am I nice ?! ", nice)*/
+	fmt.Println("Am I nice ?! ", nice)
   	sendSuccess(requestID,"{}")
 }
 
@@ -92,30 +82,144 @@ func LightOff(requestID string, input map[string]interface{}) {
 
 func Explosion(requestID string, input map[string]interface{}) {
 	fmt.Println("EXPLOSION !! (start)")
-	//fmt.Println("My charge is ", input["charge"])
+	fmt.Println("My charge is ", input["charge"])
 	sendSuccess(requestID,"{}")
 }
 
-func Cancel(requestID string) {
+func Enter(requestID string, entityID string, input map[string]interface{}) {
+	fmt.Println("Someone is entering in the room")
+	knowledge := getEntityKnowledge(entityID)
+
+	people := knowledge["people"].(float64) + 1
+
+	output := `{"result":`+strconv.FormatInt(int64(people),10)+`}`
+
+	sendSuccess(requestID,output)
+}
+
+func Exit(requestID string, entityID string, input map[string]interface{}) {
+	fmt.Println("Someone is exiting the room")
+	knowledge := getEntityKnowledge(entityID)
+
+	people := knowledge["people"].(float64) - 1
+
+	output := `{"result":`+strconv.FormatInt(int64(people),10)+`}`
+
+	sendSuccess(requestID,output)
+}
+
+func Cancel() {
 	fmt.Println("(cancel)")
-	sendCancel(requestID)
 }
 
 func getEntityKnowledge(entityID string) map[string]interface{}{
-   	body :=	Request("GET", httpURL + "/" + instanceID + "/entities/" + entityID + "/knowledge", "")
-   	var f interface{}
-    err := json.Unmarshal(body,&f)
-    if err != nil {panic(err)}
+	request, err := http.NewRequest("GET", httpURL + "/" + instanceID + "/entities/" + entityID + "/knowledge", nil)
+	if err != nil {panic(err)}
+	request.Header.Set("content-type", "application/json; charset=utf-8");
+	request.Header.Set("accept", "");
+	request.Header.Set("X-Craft-Ai-App-Id", TUTO_APP_ID);
+	request.Header.Set("X-Craft-Ai-App-Secret", TUTO_APP_SECRET);
+	client := &http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {panic(err)}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	if(sysou){
+	    fmt.Println("response Status:", resp.Status)
+	    fmt.Println("response Headers:", resp.Header)
+	    fmt.Println("response Body:", string(body))
+	}
+	var f interface{}
+    err = json.Unmarshal(body,&f)
    	m := f.(map[string]interface{})
    	return m
 }
 
 func UpdateEntityKnowledge(entityID string, know Knowledge) {
-	Request("POST",httpURL + "/" + instanceID + "/entities/" + entityID + "/knowledge", know)
+	jsonStr, err := json.Marshal(know)
+
+	var test Knowledge 
+	err = json.Unmarshal(jsonStr,&test)
+	fmt.Println("TEST = ",test)
+
+	request, err := http.NewRequest("POST", httpURL + "/" + instanceID + "/entities/" + entityID + "/knowledge", bytes.NewBuffer(jsonStr))
+	if err != nil {panic(err)}
+	request.Header.Set("content-type", "application/json; charset=utf-8");
+	request.Header.Set("accept", "");
+	request.Header.Set("X-Craft-Ai-App-Id", TUTO_APP_ID);
+	request.Header.Set("X-Craft-Ai-App-Secret", TUTO_APP_SECRET);
+	client := &http.Client{}
+    resp, err := client.Do(request)
+    if err != nil {panic(err)}
+    defer resp.Body.Close()
+    if(sysou){
+	    body, _ := ioutil.ReadAll(resp.Body)
+	    fmt.Println("response Status:", resp.Status)
+	    fmt.Println("response Headers:", resp.Header)
+	    fmt.Println("response Body:", string(body))
+	   	fmt.Println("KNOWLEDGE CHANGED !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+	}
+}
+
+
+type Knowledge struct {
+	Destination string
+	Value interface {}
 }
 
 func UpdateGlobalKnowledge(know Knowledge){
-	Request("POST", httpURL + "/" + instanceID + "/knowledge", know)
+	jsonStr, err := json.Marshal(know)
+
+
+	request, err := http.NewRequest("POST", httpURL + "/" + instanceID + "/knowledge", bytes.NewBuffer(jsonStr))
+	if err != nil {panic(err)}
+	request.Header.Set("content-type", "application/json; charset=utf-8");
+	request.Header.Set("accept", "");
+	request.Header.Set("X-Craft-Ai-App-Id", TUTO_APP_ID);
+	request.Header.Set("X-Craft-Ai-App-Secret", TUTO_APP_SECRET);
+	client := &http.Client{}
+    resp, err := client.Do(request)
+    if err != nil {panic(err)}
+    defer resp.Body.Close()
+    if(sysou){
+	    body, _ := ioutil.ReadAll(resp.Body)
+	    fmt.Println("response Status:", resp.Status)
+	    fmt.Println("response Headers:", resp.Header)
+	    fmt.Println("response Body:", string(body))
+	}
+
+}
+
+func InitRoute(){
+	for key, fnc := range actions {
+		key := key
+		fnc := fnc
+    	http.HandleFunc("/actions/"+key+"/start", 
+    		func(w http.ResponseWriter, r *http.Request) {
+				body, err := ioutil.ReadAll(r.Body)
+				if err != nil {
+		        	panic(err)
+		    	}
+		    	var f interface{}
+		    	err = json.Unmarshal(body,&f)
+		   		m := f.(map[string]interface{})
+
+		   		requestID64 := m["requestId"].(float64)	
+		   		requestID := strconv.FormatInt(int64(requestID64), 10)
+
+		   		input := m["input"].(map[string]interface{})
+
+		   		//fmt.Println("Response from the entity : ",m["entityId"].(float64))
+				fnc(requestID,input)
+			})
+		
+		http.HandleFunc("/actions/"+key+"/cancel", func(w http.ResponseWriter, r *http.Request) {
+			Cancel()
+		})
+
+
+		fmt.Println("The action "+key+" is routed with the function : ", fnc)
+	}
 }
 
 
@@ -163,47 +267,129 @@ func InitNgrok(){
 
 // Create an instance
 func CreateInstance(user string, project string, version string){
-	body := Request("PUT",httpURL + "?" + "scope=app","")
-	instanceID = string(body);
+	request, err := http.NewRequest("PUT",httpURL + "?" + "scope=app", nil)
+	if err != nil {
+        panic(err)
+    }
+	request.Header.Set("content-type", "application/json; charset=utf-8");
+	request.Header.Set("accept", "");
+	request.Header.Set("X-Craft-Ai-App-Id", TUTO_APP_ID);
+	request.Header.Set("X-Craft-Ai-App-Secret", TUTO_APP_SECRET);
+
+	client := &http.Client{}
+    resp, err := client.Do(request)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+    body, _ := ioutil.ReadAll(resp.Body)
+    instanceID = string(body);
+
+    if(sysou){
+	    fmt.Println("response Status:", resp.Status)
+	    fmt.Println("response Headers:", resp.Header)
+	    fmt.Println("response Body:", string(body))
+	}
 } 
 
-type KnowledgeJSON struct {
-	People int `json:"people"`
-}
+//Create an entity
+func CreateEntity(){
+    var jsonStr = []byte(`{"behavior":"main.bt","knowledge":{"people":0}}`)
+  	request, err := http.NewRequest("PUT", httpURL + "/" + instanceID + "/entities", bytes.NewBuffer(jsonStr))
+  	fmt.Println("HTTP CREATION ENTITY:", request.URL)
 
-type Entity struct {
-	Behavior string `json:"behavior"`
-	Knowledge KnowledgeJSON `json:"knowledge"`
-}
-
-//Creates an entity and return its ID
-func CreateEntity() string{
-	subm := KnowledgeJSON{People:0}
-	m := Entity{Behavior:"main.bt",Knowledge:subm}
-	body := Request("PUT",httpURL + "/" + instanceID + "/entities",m)
-	return string(body)
+	if err != nil {
+        panic(err)
+    }
+	request.Header.Set("content-type", "application/json; charset=utf-8");
+	request.Header.Set("accept", "");
+	request.Header.Set("X-Craft-Ai-App-Id", TUTO_APP_ID);
+	request.Header.Set("X-Craft-Ai-App-Secret", TUTO_APP_SECRET);
+	
+	client := &http.Client{}
+    resp, err := client.Do(request)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+    if(sysou){
+	    body, _ := ioutil.ReadAll(resp.Body)
+	    fmt.Println("response Status:", resp.Status)
+	    fmt.Println("response Headers:", resp.Header)
+	    fmt.Println("response Body:", string(body))
+	}
 }
 
 
 func UpdateCraft(){
-	Request("POST",httpURL + "/" + instanceID + "/update",`{"time":"0.5","ts":` + strconv.FormatInt(time.Now().Unix(),10) +`}`)
+	var jsonStr = []byte(`{"time":"0.5","ts":` + strconv.FormatInt(time.Now().Unix(),10) +`}`)
+  	request, err := http.NewRequest("POST", httpURL + "/" + instanceID + "/update", bytes.NewBuffer(jsonStr))
+	if err != nil {
+        panic(err)
+    }
+	request.Header.Set("content-type", "application/json; charset=utf-8");
+	request.Header.Set("accept", "");
+	request.Header.Set("X-Craft-Ai-App-Id", TUTO_APP_ID);
+	request.Header.Set("X-Craft-Ai-App-Secret", TUTO_APP_SECRET);
+	
+	client := &http.Client{}
+    resp, err := client.Do(request)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+   	if(sysou){
+	    body, _ := ioutil.ReadAll(resp.Body)
+	    fmt.Println("response Status:", resp.Status)
+	    fmt.Println("response Headers:", resp.Header)
+	    fmt.Println("response Body:", string(body))
+	}
 }
 
-
-type ActionJSON struct {
-	Name string `json:"name"`
-	Url string `json:"url"`
-}
 //Register an action 
 func RegisterAction(actionID string){
-	m := ActionJSON{Name:actionID,Url:ngrok_url+"/actions/"+ actionID +"/"}
-	Request("PUT", httpURL + "/" + instanceID + "/actions",m)
+	var jsonStr = []byte(`{"name":"`+ actionID +`","url":"`+ngrok_url+`/actions/`+ actionID +`/"}`)
+  	request, err := http.NewRequest("PUT", httpURL + "/" + instanceID + "/actions", bytes.NewBuffer(jsonStr))
+	if err != nil {
+        panic(err)
+    }
+	request.Header.Set("content-type", "application/json; charset=utf-8");
+	request.Header.Set("accept", "");
+	request.Header.Set("X-Craft-Ai-App-Id", TUTO_APP_ID);
+	request.Header.Set("X-Craft-Ai-App-Secret", TUTO_APP_SECRET);
+	
+	client := &http.Client{}
+    resp, err := client.Do(request)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
+    if(sysou){
+	    body, _ := ioutil.ReadAll(resp.Body)
+	    fmt.Println("response Status:", resp.Status)
+	    fmt.Println("response Headers:", resp.Header)
+	    fmt.Println("response Body:", string(body))
+	}
 }
 
 
 // Delete an instance with its ID
 func DeleteInstance(){
-    Request("DELETE",httpURL + "/" + instanceID, "")
+	request, err := http.NewRequest("DELETE",httpURL + "/" + instanceID, nil)
+	if err != nil {
+        panic(err)
+    }
+	request.Header.Set("content-type", "application/json; charset=utf-8");
+	request.Header.Set("accept", "");
+	request.Header.Set("X-Craft-Ai-App-Id", TUTO_APP_ID);
+	request.Header.Set("X-Craft-Ai-App-Secret", TUTO_APP_SECRET);
+
+	client := &http.Client{}
+    resp, err := client.Do(request)
+    if err != nil {
+        panic(err)
+    }
+    defer resp.Body.Close()
 }
 
 func HandleCTRL_C(){
@@ -219,11 +405,11 @@ func HandleCTRL_C(){
 	}()
 }
 
-func RandomHumanInteraction(entityID string){
+func RandomHumanInteraction(){
 	c := time.Tick(1000 * time.Millisecond)
     for _ = range c {
     	r := rand.Intn(10)
-    	knowledge := getEntityKnowledge(entityID) // Get the entity Knowledge to modify the number of people in the room
+    	knowledge := getEntityKnowledge("0") // Get the entity Knowledge to modify the number of people in the room
     	people := knowledge["people"].(float64)
     	if r <4 {
     		fmt.Println("A man is entering")
@@ -237,54 +423,11 @@ func RandomHumanInteraction(entityID string){
 
     	fmt.Println("Total of men : ",people)
 
-    	peopleKnowledge := make(Knowledge)
-    	peopleKnowledge["people"] = people
-
-    	UpdateEntityKnowledge(entityID,peopleKnowledge)
+    	know := Knowledge{"people",int64(people)}
+    	fmt.Println("***********************************",know)
+    	UpdateEntityKnowledge("0",know)
     	fmt.Println("")
     }
-}
-
-func InitRoute(){
-	for key, fnc := range actions {
-		key := key
-		fnc := fnc
-    	http.HandleFunc("/actions/"+key+"/start", 
-    		func(w http.ResponseWriter, r *http.Request) {
-				body, err := ioutil.ReadAll(r.Body)
-				if err != nil {
-		        	panic(err)
-		    	}
-		    	var f interface{}
-		    	err = json.Unmarshal(body,&f)
-		   		m := f.(map[string]interface{})
-
-		   		requestID64 := m["requestId"].(float64)	
-		   		requestID := strconv.FormatInt(int64(requestID64), 10)
-
-		   		input := m["input"].(map[string]interface{})
-
-		   		//fmt.Println("Response from the entity : ",m["entityId"].(float64))
-				fnc(requestID,input)
-			})
-		
-		http.HandleFunc("/actions/"+key+"/cancel", func(w http.ResponseWriter, r *http.Request) {
-			body, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-	        	panic(err)
-	    	}
-	    	var f interface{}
-	    	err = json.Unmarshal(body,&f)
-	   		m := f.(map[string]interface{})
-
-	   		requestID64 := m["requestId"].(float64)	
-	   		requestID := strconv.FormatInt(int64(requestID64), 10)
-			Cancel(requestID)
-		})
-
-
-		fmt.Println("The action "+key+" is routed with the function : ", fnc)
-	}
 }
 
 
@@ -306,22 +449,26 @@ func main() {
 
 	actions = map[string]ActionFunction {"light": Light, "explosion": Explosion, "lightoff": LightOff}
 
+
+
 	go InitNgrok()
 
 	fmt.Println("")
 	CreateInstance(user, project, version)
 	fmt.Println("")
 
-	entityID := CreateEntity();
+	CreateEntity();
 	fmt.Println("")
 
-	for key, _ := range actions {
-		RegisterAction(key)
-	}
+	RegisterAction("light")
+	RegisterAction("enter")
+	RegisterAction("exit")
+	RegisterAction("explosion")
+	RegisterAction("lightoff")
 
 	HandleCTRL_C()
 
-	go RandomHumanInteraction(entityID);
+	go RandomHumanInteraction();
 
     c := time.Tick(500 * time.Millisecond)
     for _ = range c {
